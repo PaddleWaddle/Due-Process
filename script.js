9483a12a121e2e3247d8997df7e842b4ba92656b
PaddleWaddle <98291284+PaddleWaddle@users.noreply.github.com>
2022-10-29 16:38:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function openPage() {
-            var x = document.getElementById("search").value;
+          var x = document.getElementById("search").value;
 
-            if (x === "Home") {
-                window.open("index.html");
-            }
+          if (x === "Home") {
+              window.open("https://paddlewaddle.github.io/Due-Process/index.html");
           }
+          if (x === "Rights") {
+              window.open("https://paddlewaddle.github.io/Due-Process/Rights.html");
+          }
+          if (x === "History") {
+              window.open("https://paddlewaddle.github.io/Due-Process/History.html");
+          }
+          if (x === "School") {
+              window.open("https://paddlewaddle.github.io/Due-Process/School.html");
+          }
+          if (x === "More") {
+              window.open("https://paddlewaddle.github.io/Due-Process/More.html");
+          }
+        }
